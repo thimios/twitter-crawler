b7c1759404e8aa05c2c768b1ff50a7d37a5e9e4b
Thimios Dimopulos <thimios@wantedpixel.com>
2023-10-15 15:44:24 +0300

    switch to latest ruby and add active_interaction gem

--- scanner/app/controllers/scans_controller.rb
+++ scanner/app/controllers/scans_controller.rb
@@ -2,6 +2,23 @@
 
 class ScansController < ApplicationController
   def index
-    @scans = Scan.all
+    @scans = ListScans.run!
+  end
+
+  # GET /accounts/new
+  def new
+    @account = CreateScan.new
+  end
+
+  # POST /scans
+  def create
+    outcome = CreateScan.run(params.fetch(:scan, {}))
+
+    if outcome.valid?
+      redirect_to(outcome.result)
+    else
+      @scan = outcome
+      render(:new)
+    end
   end
 end
